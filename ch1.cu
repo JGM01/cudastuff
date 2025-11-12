8f2d7bb714409279ff63792af6f6131e3d8b5e03
JGM01 <jacobgermanamccray@gmail.com>
2025-11-11 20:36:56 -0500

    some notes on threadidx/blockidx/blockdim

--- ch1.cu
+++ ch1.cu
@@ -1,6 +1,29 @@
 #include <stdio.h>
 #include <vector>
 
+// threadIdx is the index of a given thread within a BLOCK, upper bounded by blockDim
+// - This means 2 threads could have the same threadIdx when in different blocks
+// blockIdx is the index of a given block within a GRID
+// blockDim is the total # of threads that go into a block
+// - so if blockDim = 256, then the highest threadIdx is 255
+
+// All of these index identifiers have x y & z fields (dependent on how you wanna interpret the flat data)
+// - treating them as normal flat arrays is x, 2D is y, 3D is z
+
+// blockIdx = area code
+// threadIdx = local phone number
+
+// can construct a unique global index w/ blockIdx & threadIdx
+// for example globalIdx = blockIdx.x * blockDim + threadIdx.x
+// this basically gets the index by jumping through the giant array by how many blocks/how large each block is + the thread index
+
+// this gets ran per thread (a lot)
+__global__
+void vecAddKernel(float* a, float* b, float* c, int n) {
+	int globalIdx = threadIdx.x + blockIdx.x * blockDim.x;
+	c[globalIdx] = a[globalIdx] + b[globalIdx];
+}
+
 void vecAdd(std::vector<float> a_h, std::vector<float> b_h, std::vector<float> c_h) {
 	size_t n = c_h.size();
 
@@ -16,7 +39,7 @@ void vecAdd(std::vector<float> a_h, std::vector<float> b_h, std::vector<float> c
 	cudaMemcpy(b_d, b_h.data(), n, cudaMemcpyHostToDevice);
 	
 	// kernel logic goes here ***
-	
+	vecAddKernel(a_d, b_d, c_d, n);
 
 	cudaMemcpy(c_h.data(), c_d, n, cudaMemcpyDeviceToHost);
 	cudaFree(a_d);
